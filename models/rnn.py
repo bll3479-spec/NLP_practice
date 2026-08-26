872d111f194610b1082060297127f788951aa2a8
SH Kim <bll3479@gmail.com>
2026-08-26 15:42:04 +0900

    +0826: 1 epoch

--- models/rnn.py
+++ models/rnn.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 PAD_IDX = 0
 
-
 class SpamRNN(nn.Module):
     def __init__(self, vocab_size, embed_size = 64, hidden_size = 128, num_layers = 3, 
                  dropout = 0.3, num_classes = 2):
+        super().__init__()
         #nn.Embedding (vocab_size, embed_size): input 개수, output 개수;
         # input 개수: vocab_size, n개의 단어들을 갖고 있다는 뜻
         # output 개수: embed_size(embed_dim), 몇차원 공간
