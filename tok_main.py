cbcb8f3399a6966eaa3369a3f2f4425504182c28
SH Kim <bll3479@gmail.com>
2026-08-27 17:21:14 +0900

    +0827: tok_main.py

--- tok_main.py
+++ tok_main.py
@@ -38,8 +38,13 @@ def make_splits(length_of_df):
     split_num = torch.randperm(length_of_df, generator = torch.Generator().manual_seed(42)).tolist()
     n_train = int(length_of_df * 0.7)
     n_valid = int(length_of_df * 0.15)
-    return split_num[:n_train], split_num[n_train:n_train+n_valid, split_num[n_train+n_valid]]
+    return split_num[:n_train], split_num[n_train:n_train+n_valid], split_num[n_train+n_valid:]
 
+import torch.nn as nn
+import torch.optim as optim
+from models.rnn import SpamRNN, SpamLSTM, SpamGRU
+from utils.visualize import plot_comparison
+from main import train, evaluate
 
 if __name__ == '__main__':
 
@@ -50,23 +55,62 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
     #0. 전체 데이터셋인 df 가져옴
-    df = pd.read_csv('SMSSpamCollection', sep = '\t', header = None, names = ['labels','text'])
+    df = pd.read_csv('SMSSpamCollection', sep = '\t', header = None, names = ['label','text'])
     df['label'] = (df['label'] == 'spam').astype(int)       #데이터프레임 열 중 label 열을 0(정상), 1(스팸)으로 분류
 
-    train_idx, valid_idx, test_idx =make_splits(len(df)) 
+    print(df.head())
+
+    train_idx, valid_idx, test_idx = make_splits(len(df)) 
 
     #1. 전통적인 tokenizer를 거친 데이터셋을 갖고 와 lstm으로 훈련
+    #1-1. 전통적인 tokenizer를 거친 데이터셋 로드
+    original_vocab = main.build_vocab(df)
+    original_data = main.SpamDataset(df, original_vocab, 50)
+
+    original_train = DataLoader(Subset(original_data, train_idx), batch_size=32, shuffle=True)
+    original_valid = DataLoader(Subset(original_data, valid_idx), batch_size=32)
+    original_test = DataLoader(Subset(original_data, test_idx), batch_size=32)
+
+    td, tl = next(iter(original_test))
+    print(td, tl)
+
+    #1-2. 모델 생성
+    original_LSTM = SpamLSTM(len(original_vocab))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(original_LSTM.parameters())
+    num_epochs = 30
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #훈련, 평가
+    original_history = train(original_LSTM, original_train, original_valid, criterion, optimizer, num_epochs, device, model_name = 'original')
+    o_labels, o_preds = evaluate(original_LSTM, original_test, device, model_name = 'original')
+
 
     #2. 허깅페이스 tokenizer를 거친 데이터셋을 갖고 와 lstm으로 훈련
+    #2-1.허깅페이스를 거친 데이터셋 로드
+    hug_data = HugDataset(df, tokenizer, 50)
+    hug_train = DataLoader(Subset(hug_data, train_idx), batch_size=32, shuffle=True)
+    hug_valid = DataLoader(Subset(hug_data, valid_idx), batch_size=32)
+    hug_test = DataLoader(Subset(hug_data, test_idx), batch_size=32)
 
+    #2-2. 모델 생성, 훈련 및 평가
+    #허깅페이스에서 다운받은 토크나이저의 특성 속에 vocab_size가 미리 정의되어 있음
+    hug_LSTM = SpamLSTM(tokenizer.vocab_size)
+    h_optimizer = optim.Adam(hug_LSTM.parameters())
 
+    hug_history = train(hug_LSTM, hug_train, hug_valid, criterion, h_optimizer, num_epochs, device, model_name='hugging_face')
+    h_labels, h_preds = evaluate(hug_LSTM, hug_test, device, model_name = 'hugging_face')
 
+    histories, eval_results, train_models = [], [], {}
 
+    histories.append(original_history)
+    histories.append(hug_history)
+    eval_results.append((o_labels, o_preds))
+    eval_results.append((h_labels, h_preds))
+    train_models['original'] = original_LSTM
+    train_models['hugging_face'] = hug_LSTM
 
-
-
-
-
+    plot_comparison(histories, ['original', 'hugging_face'])
 
 
 
